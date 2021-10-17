1ea6ee4eef01b56ee9d5ccc59378b7a4e0d5e493
tcyang1971 <tcyang@gm.pu.edu.tw>
2021-10-17 15:59:30 +0800

    version 3c: flask (html datetime)

--- tc_flask.py
+++ tc_flask.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,7 +9,12 @@ def index():
 
 @app.route("/mis")
 def course():
-    return "<h1>資訊管理導論</h1>"    
+    return "<h1>資訊管理導論</h1>" 
+    
+@app.route("/current")
+def current():
+    now = datetime.now()
+    return render_template("current.html", datetime = str(now))   
 
 if __name__ == "__main__":
     app.run()
