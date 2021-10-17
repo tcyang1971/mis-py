6b58caa3258bdd31e6275e009f427a4b868ef30a
tcyang1971 <tcyang@gm.pu.edu.tw>
2021-10-17 16:08:44 +0800

    version 3e: flask (counter)

--- tc_flask.py
+++ tc_flask.py
@@ -21,5 +21,21 @@ def welcome():
     user = request.values.get("nick")
     return render_template("welcome.html", name=user)  
 
+@app.route("/hi")
+def hi():
+    # 載入原始檔案
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+
+    # 計數加1
+    count += 1
+
+    # 覆寫檔案
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
+    return "本網站總拜訪人次：" + str(count)
+
 if __name__ == "__main__":
     app.run()
